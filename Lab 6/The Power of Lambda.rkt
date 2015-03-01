7e844a413256630b1253b04a1f62241aa93b0523
LampeMW <lampemw@gmail.com>
2015-02-28 17:47:05 -0800

    Added personal info

--- Lab 6/The Power of Lambda.rkt
+++ Lab 6/The Power of Lambda.rkt
@@ -1,6 +1,22 @@
+#|
+## Lab 6 - The Power of Lambda
+## Matt Lampe
+## M03516707
+## CS4003
+## 2/28/2015
+## Description:  Uses an infinite list and lazy procedures to find the first N prime numbers
+## Usage - Run program and enter (stol N) to find the first N prime numbers
+|#
+
 (define int-builder$
   (lambda (n)
     (cons n (lambda () (int-builder$ (+ n 1))))))
+
+(define take$
+  (lambda (m s)
+    (if (or (= m 0) (null? s))
+        '()
+        (cons (car s) (take$ (- m 1) ((cdr s)))))))
 
 (define filter-out-mults$
   (lambda (num s)
@@ -10,13 +26,11 @@
             (filter-out-mults$ num ((cdr s)))
             (cons (car s) (lambda () (filter-out-mults$ num ((cdr s)))))))))
 
-(define take$
-  (lambda (m s)
-    (if (or (= m 0) (null? s))
-        '()
-        (cons (car s) (take$ (- m 1) ((cdr s)))))))
-
 (define sieve$
   (lambda (s)
-    (list (car s)
-          (lambda () (sieve$ (filter-out-mults$ (car s) ((cadr s))))))))
+    (cons (car s)
+          (lambda () (sieve$ (filter-out-mults$ (car s) ((cdr s))))))))
+
+(define stol$
+  (lambda (n)
+    (take$ n (sieve$ (int-builder$ 2)))))
